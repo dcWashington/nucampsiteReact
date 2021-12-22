0108b92577175677b1d7165f605ab3f7f04149a3
Washington Luis <washingtonluismm@gmail.com>
2021-12-21 23:58:13 -0800

    xpm Presentational and Container Components w ue

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
 class CampsiteInfo extends Component{
 
     renderCampsite(campsite){
@@ -41,23 +40,18 @@ class CampsiteInfo extends Component{
         );
     }
 
-    render(){
-        if (this.props.campsite){
-            return(
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
-            )
-        }
-        else {
+    render() {
+        if (this.props.campsite) {
             return (
-            <div>
-                
-            </div>
-            )
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
         }
+        return <div />;
     }
-}
 
 export default CampsiteInfo;
