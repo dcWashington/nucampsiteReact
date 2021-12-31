240fbc1d7d4192dbaa51e5c89a257750a816e3b4
Washington Luis <washingtonluismm@gmail.com>
2021-12-30 18:26:26 -0800

    xpm 2/3 Adding to the Single Page App

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
     return (
