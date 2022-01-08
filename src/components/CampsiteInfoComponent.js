479caa46fd5a4b3eb2b436b684ced41416f89b7d
Washington Luis <washingtonluismm@gmail.com>
2022-01-07 19:23:35 -0800

    xp ws w4 2-3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,66 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values){
+        alert(JSON.stringify(values));
+    }
+
+    render(){
+
+        return(
+            <div>
+                <Button outline onClick={this.toggleModal}><i className="fa fa-pencil fa-lg"/> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}> Submit Comment 
+
+                    </ModalHeader>
+                    <LocalForm style={{padding:10}} onSubmit={this.handleSubmit}>
+                        <div className="form-group" > Rating
+                            <Control.select className="form-control" model=".rating" id="rating" name="rating">
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                            </Control.select>
+                        </div>
+                        <div className="form-group">Your Name
+                            <Control.text className="form-control" model=".author" id="author" name="author" placeholder="Your Name">
+
+                            </Control.text>
+                        </div>
+                        <div className="form-group">Comment
+                            <Control.textarea className="form-control" rows="6" model=".text" id="text" name="text" >
+
+                            </Control.textarea>
+                        </div>
+                        <Button type="submit" value="submit" color="primary">Submit</Button>
+                    </LocalForm>
+                </Modal>
+            </div>
+        );
+    }
+}
+
 
 function RenderCampsite ({campsite}) {
     return(
@@ -14,17 +74,19 @@ function RenderCampsite ({campsite}) {
         </div>
     );
 }
+
 function RenderComments({comments}){
     if (comments){
         return(
             <div className="col-md-5 m-1">
-                <h4>(Only the first 5?) Comments</h4>
+                <h4>Comments</h4>
                 {comments.map(comment => {
                     const date = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)));
                     return(
                         <p key={comment.id}>{comment.text}<br />-- {comment.author}, {date}</p>
                     );
                 })}
+                <CommentForm />
             </div>
         );
     }
@@ -58,6 +120,7 @@ function CampsiteInfo(props) {
     }
     return <div />;
 }
+
 
 
 export default CampsiteInfo;
