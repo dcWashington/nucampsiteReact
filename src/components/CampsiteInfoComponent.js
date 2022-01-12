593680f1ca11a5578cf89ecd0e65f9eabbb66b8c
Washington Luis <washingtonluismm@gmail.com>
2022-01-11 20:41:07 -0800

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,9 +25,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values){
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render(){
@@ -110,7 +109,7 @@ function RenderCampsite ({campsite}) {
     );
 }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
     if (comments){
         return(
             <div className="col-md-5 m-1">
@@ -121,7 +120,7 @@ function RenderComments({comments}){
                         <p key={comment.id}>{comment.text}<br />-- {comment.author}, {date}</p>
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -148,7 +147,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}    
+                    />
                 </div>
             </div>
         );
