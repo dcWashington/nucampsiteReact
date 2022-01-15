f52f612e767a12f0d3b4a24eba0a1b1fe6c53cfb
Washington Luis <washingtonluismm@gmail.com>
2022-01-14 20:55:16 -0800

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -25,8 +26,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values){
-        this.toggleModal();
         this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.toggleModal();
     }
 
     render(){
@@ -132,6 +133,26 @@ function RenderComments({comments, addComment, campsiteId}){
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
