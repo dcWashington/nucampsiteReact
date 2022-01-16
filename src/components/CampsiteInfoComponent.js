806f4bc01544f2623d1a06d17484622f24aabea9
Washington Luis <washingtonluismm@gmail.com>
2022-01-15 21:38:31 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,9 +26,9 @@ class CommentForm extends Component {
         });
     }
 
-    handleSubmit(values){
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    handleSubmit(values) {
         this.toggleModal();
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render(){
@@ -111,7 +111,7 @@ function RenderCampsite ({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}){
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments){
         return(
             <div className="col-md-5 m-1">
@@ -122,7 +122,7 @@ function RenderComments({comments, addComment, campsiteId}){
                         <p key={comment.id}>{comment.text}<br />-- {comment.author}, {date}</p>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -169,11 +169,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
-                        campsiteId={props.campsite.id}    
-                    />
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id}
+                    />      
                 </div>
             </div>
         );
